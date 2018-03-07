3f26b3d800e5a782b3fbf35819cd7418b13c6389
pamsfields <jh5811zj@go.minneapolis.edu>
2018-03-06 19:20:51 -0600

    Delete petpriceprogram.py

--- petpriceprogram.py
+++
@@ -1,5 +0,0 @@
-animal_wanted=input("Which animal would you want to own? Please type cat, dog, fish, small bird, or small rodent: ")
-animal_age=input("How old is this animal in years?: ")
-animal_location=input("Where did you purchase the animal from? Please type shelter, private home, or breeder: ")
-animal_altered=input("If this is a dog or cat, is the animal spayed or neutered? Please type yes or no: ")
-declawing=input("If this animal is a cat, does your apartment require your cat to be declawed? Please type yes or no: ")
